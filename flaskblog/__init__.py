5a8c31442b16cf4168170c12834c0d06526bf0f1
minatosan <yuuki3yuki3@yahoo.co.jp>
2022-01-22 14:04:31 +0900

    dotenv追加

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,9 +1,12 @@
 # __init__.py
 import os
+from os.path import join, dirname
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+from flask_dance.contrib.google import make_google_blueprint
+from dotenv import load_dotenv
 
 login_manager = LoginManager()
 login_manager.login_view = 'app.view'
@@ -14,10 +17,24 @@ db = SQLAlchemy()
 migrate = Migrate()
 app = Flask(__name__)
 
+load_dotenv(verbose=True)
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+google_blueprint = make_google_blueprint(
+    client_id=os.environ.get("GOOGLE_ID"),
+    client_secret=os.environ.get("GOOGLE_KEY"),
+    scope=[
+        "https://www.googleapis.com/auth/plus.me",
+        "https://www.googleapis.com/auth/userinfo.email",
+    ],
+    redirect_to='app.google_login'
+)
+
 
 def create_app():
     
-    app.config['SECRET_KEY'] = 'mysite'
+    app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
     app.config['SQLALCHEMY_DATABASE_URI'] = \
         'postgresql+psycopg2://test01:test01@db:5432/test01'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
